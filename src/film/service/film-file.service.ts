06ac84db270fe88e870f0e6d818b40c50b05df17
Lisella <maus.lisa@gmx.de>
2020-11-12 14:22:28 +0100

    no message

---
+++ src/film/service/film-file.service.ts
@@ -0,0 +1,169 @@
+/*
+ * Copyright (C) 2017 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { FilmNotExists, FileNotFound, MultipleFiles } from './errors';
+import { closeMongoDBClient, connectMongoDB, saveReadable } from '../../shared';
+import { FilmModel } from '../entity';
+import { GridFSBucket } from 'mongodb';
+import JSON5 from 'json5';
+import type { ObjectId } from 'mongodb';
+import { Readable } from 'stream';
+import { logger } from '../../shared';
+
+/* eslint-disable unicorn/no-useless-undefined */
+export class FilmFileService {
+    async save(id: string, buffer: Buffer, contentType: string | undefined) {
+        logger.debug(
+            `FilmFileService.save(): id = ${id}, contentType=${contentType}`,
+        );
+
+        // Gibt es einen Film zur angegebenen ID?
+        const film = await FilmModel.findById(id);
+        // eslint-disable-next-line no-null/no-null
+        if (film === null) {
+            return false;
+        }
+
+        const { db, client } = await connectMongoDB();
+        const bucket = new GridFSBucket(db);
+        await this.deleteFiles(id, bucket);
+
+        // https://stackoverflow.com/questions/13230487/converting-a-buffer-into-a-readablestream-in-node-js#answer-44091532
+        const readable = new Readable();
+        // _read ist erforderlich, kann die leere Funktion sein
+        readable._read = () => {}; // eslint-disable-line no-underscore-dangle,no-empty-function
+        readable.push(buffer);
+        readable.push(null); // eslint-disable-line no-null/no-null,unicorn/no-null
+
+        const metadata = { contentType };
+        saveReadable(readable, bucket, id, { metadata }, client);
+        return true;
+    }
+
+    async find(filename: string) {
+        logger.debug(`FilmFileService.findFile(): filename=${filename}`);
+        const resultCheck = await this.checkFilename(filename);
+        if (resultCheck !== undefined) {
+            return resultCheck;
+        }
+
+        const { db, client } = await connectMongoDB();
+
+        // https://mongodb.github.io/node-mongodb-native/3.5/tutorials/gridfs/streaming
+        const bucket = new GridFSBucket(db);
+        const resultContentType = await this.getContentType(filename, bucket);
+        if (typeof resultContentType !== 'string') {
+            return resultContentType;
+        }
+
+        const contentType = resultContentType;
+        // https://mongodb.github.io/node-mongodb-native/3.5/tutorials/gridfs/streaming/#downloading-a-file
+        // https://www.freecodecamp.org/news/node-js-streams-everything-you-need-to-know-c9141306be93
+        const readStream = bucket
+            .openDownloadStreamByName(filename)
+            .on('end', () => closeMongoDBClient(client));
+        return { readStream, contentType };
+    }
+
+    private async deleteFiles(filename: string, bucket: GridFSBucket) {
+        logger.debug(`FilmFileService.deleteFiles(): filename=${filename}`);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/naming-convention
+        const idObjects: { _id: ObjectId }[] = await bucket
+            .find({ filename })
+            .project({ _id: 1 }) // eslint-disable-line @typescript-eslint/naming-convention
+            .toArray();
+        const ids = idObjects.map((obj) => obj._id);
+        logger.debug(
+            `FilmFileService.deleteFiles(): ids=${JSON5.stringify(ids)}`,
+        );
+        ids.forEach((fileId) =>
+            bucket.delete(fileId, () =>
+                logger.debug(
+                    `FilmFileService.deleteFiles(): geloeschte ID=${JSON5.stringify(
+                        fileId,
+                    )}`,
+                ),
+            ),
+        );
+    }
+
+    private async checkFilename(filename: string) {
+        logger.debug(`FilmFileService.checkFilename(): filename=${filename}`);
+
+        // Gibt es einen Film mit dem gegebenen "filename" als ID?
+        const film = await FilmModel.findById(filename);
+        // eslint-disable-next-line no-null/no-null
+        if (film === null) {
+            const result = new FilmNotExists(filename);
+            logger.debug(
+                `FilmFileService.checkFilename(): FilmNotExists=${JSON5.stringify(
+                    result,
+                )}`,
+            );
+            return result;
+        }
+
+        logger.debug(
+            `FilmFileService.checkFilename(): film=${JSON5.stringify(film)}`,
+        );
+
+        return undefined;
+    }
+
+    private async getContentType(filename: string, bucket: GridFSBucket) {
+        let files: { metadata: { contentType: string } }[];
+        try {
+            files = await bucket.find({ filename }).toArray(); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+        } catch (err: unknown) {
+            logger.error(`${JSON5.stringify(err)}`);
+            files = [];
+        }
+
+        switch (files.length) {
+            case 0: {
+                const error = new FileNotFound(filename);
+                logger.debug(
+                    `FilmFileService.getContentType(): FileNotFound=${JSON5.stringify(
+                        error,
+                    )}`,
+                );
+                return error;
+            }
+
+            case 1: {
+                const [file] = files;
+                const { contentType }: { contentType: string } = file.metadata;
+                logger.debug(
+                    `FilmFileService.getContentType(): contentType=${contentType}`,
+                );
+                return contentType;
+            }
+
+            default: {
+                const error = new MultipleFiles(filename);
+                logger.debug(
+                    `FilmFileService.getContentType(): MultipleFiles=${JSON5.stringify(
+                        error,
+                    )}`,
+                );
+                return new MultipleFiles(filename);
+            }
+        }
+    }
+}
+
+/* eslint-enable unicorn/no-useless-undefined */
---
+++ src/film/service/film.service.ts
@@ -0,0 +1,396 @@
+import type { Film, FilmData } from '../entity';
+import {
+    FilmInvalid,
+    FilmNotExists,
+    FilmServiceError,
+    IsbnExists,
+    TitelExists,
+    VersionInvalid,
+    VersionOutdated,
+} from './errors';
+import { FilmModel, validateFilm } from '../entity';
+import { dbConfig, logger, mailConfig, serverConfig } from '../../shared';
+import { FilmServiceMock } from './mock';
+import type { Document } from 'mongoose';
+import JSON5 from 'json5';
+import type { SendMailOptions } from 'nodemailer';
+import { startSession } from 'mongoose';
+
+const { mockDB } = dbConfig;
+
+
+export class FilmService {
+    private readonly mock: FilmServiceMock | undefined;
+
+    constructor() {
+        if (mockDB) {
+            this.mock = new FilmServiceMock();
+        }
+    }
+
+    // Status eines Promise:
+    // Pending: das Resultat gibt es noch nicht, weil die asynchrone Operation,
+    //          die das Resultat liefert, noch nicht abgeschlossen ist
+    // Fulfilled: die asynchrone Operation ist abgeschlossen und
+    //            das Promise-Objekt hat einen Wert
+    // Rejected: die asynchrone Operation ist fehlgeschlagen and das
+    //           Promise-Objekt wird nicht den Status "fulfilled" erreichen.
+    //           Stattdessen ist im Promise-Objekt die Fehlerursache enthalten.
+
+
+    // Film mit id suchen
+    async findById(id: string) {
+        if (this.mock !== undefined) {
+            return this.mock.findById(id);
+        }
+        logger.debug(`FilmService.findById(): id= ${id}`);
+
+        // einen Film zur gegebenen ID asynchron suchen
+        // Pattern "Active Record" (urspruengl. von Ruby-on-Rails)
+        // null falls nicht gefunden
+        // lean() liefert ein "Plain JavaScript Object" statt ein Mongoose Document
+        // so dass der virtuelle getter "id" auch nicht mehr vorhanden ist
+        const film = await FilmModel.findById(id).lean<FilmData>();
+        return film ?? undefined;
+    }
+
+    //Film mit query suchen
+    async find(query?: any | undefined) {
+        if (this.mock !== undefined) {
+            return this.mock.find(query);
+        }
+
+        logger.debug(`FilmService.find(): query=${JSON5.stringify(query)}`);
+
+        // alle Buecher asynchron suchen u. aufsteigend nach titel sortieren
+        // https://docs.mongodb.org/manual/reference/object-id
+        // entries(): { titel: 'a', rating: 5 } => [{ titel: 'x'}, {rating: 5}]
+        if (query === undefined || Object.entries(query).length === 0) {
+            logger.debug('FilmService.find(): alle Buecher');
+            // lean() liefert ein "Plain JavaScript Object" statt ein Mongoose Document
+            return FilmModel.find().sort('titel').lean<FilmData>();
+        }
+
+        // { titel: 'a', rating: 5, javascript: true }
+        const { titel, javascript, typescript, ...dbQuery } = query; // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+
+        // Buecher zur Query (= JSON-Objekt durch Express) asynchron suchen
+        if (titel !== undefined) {
+            // Titel in der Query: Teilstring des Titels,
+            // d.h. "LIKE" als regulaerer Ausdruck
+            // 'i': keine Unterscheidung zw. Gross- u. Kleinschreibung
+            // NICHT /.../, weil das Muster variabel sein muss
+            // CAVEAT: KEINE SEHR LANGEN Strings wg. regulaerem Ausdruck
+            // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+            if (titel.length < 10) {
+                dbQuery.titel = new RegExp(titel, 'iu'); // eslint-disable-line security/detect-non-literal-regexp
+            }
+        }
+
+        
+        const schlagwoerter = [];
+        if (javascript === 'true') {
+            schlagwoerter.push('JAVASCRIPT');
+        }
+        if (typescript === 'true') {
+            schlagwoerter.push('TYPESCRIPT');
+        }
+        if (schlagwoerter.length === 0) {
+            delete dbQuery.schlagwoerter;
+        } else {
+            dbQuery.schlagwoerter = schlagwoerter;
+        }
+
+        logger.debug(`FilmService.find(): dbQuery=${JSON5.stringify(dbQuery)}`);
+
+        // Pattern "Active Record" (urspruengl. von Ruby-on-Rails)
+        // leeres Array, falls nichts gefunden wird
+        // lean() liefert ein "Plain JavaScript Object" statt ein Mongoose Document
+        return FilmModel.find(dbQuery).lean<FilmData>();
+        // Film.findOne(query), falls das Suchkriterium eindeutig ist
+        // bei findOne(query) wird null zurueckgeliefert, falls nichts gefunden
+    }
+
+    async create(filmData: Film) {
+        if (this.mock !== undefined) {
+            return this.mock.create(filmData);
+        }
+
+        logger.debug(
+            `FilmService.create(): filmData=${JSON5.stringify(filmData)}`,
+        );
+        const result = await this.validateCreate(filmData);
+        if (result instanceof FilmServiceError) {
+            return result;
+        }
+
+        const film = new FilmModel(filmData);
+        let filmSaved!: Document;
+        // https://www.mongodb.com/blog/post/quick-start-nodejs--mongodb--how-to-implement-transactions
+        const session = await startSession();
+        try {
+            await session.withTransaction(async () => {
+                filmSaved = await film.save();
+            });
+        } catch (err: unknown) {
+            logger.error(
+                `FilmService.create(): Die Transaktion wurde abgebrochen: ${JSON5.stringify(
+                    err,
+                )}`,
+            );
+            // TODO [2030-09-30] Weitere Fehlerbehandlung bei Rollback
+        } finally {
+            session.endSession();
+        }
+        const filmDataSaved: FilmData = filmSaved.toObject(); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+        logger.debug(
+            `FilmService.create(): filmDataSaved=${JSON5.stringify(
+                filmDataSaved,
+            )}`,
+        );
+
+        await this.sendmail(filmDataSaved);
+
+        return filmDataSaved;
+    }
+
+    async update(filmData: Film, versionStr: string) {
+        if (this.mock !== undefined) {
+            return this.mock.update(filmData);
+        }
+
+        logger.debug(
+            `FilmService.update(): filmData=${JSON5.stringify(filmData)}`,
+        );
+        logger.debug(`FilmService.update(): versionStr=${versionStr}`);
+
+        const validateResult = await this.validateUpdate(filmData, versionStr);
+        if (validateResult instanceof FilmServiceError) {
+            return validateResult;
+        }
+
+        // findByIdAndReplace ersetzt ein Document mit ggf. weniger Properties
+        const film = new FilmModel(filmData);
+        const updateOptions = { new: true };
+        const result = await FilmModel.findByIdAndUpdate(
+            film._id,
+            film,
+            updateOptions,
+        ).lean<FilmData>();
+        if (result === null) {
+            return new FilmNotExists(film._id);
+        }
+
+        if (result.__v !== undefined) {
+            result.__v++;
+        }
+        logger.debug(`FilmService.update(): result=${JSON5.stringify(result)}`);
+
+        // Weitere Methoden von mongoose zum Aktualisieren:
+        //    Film.findOneAndUpdate(update)
+        //    film.update(bedingung)
+        return Promise.resolve(result);
+    }
+
+    async findOneAndUpdate(update: Film) {
+        if(this.mock !== undefined){
+            return this.mock.update(update);
+        }
+        logger.debug(
+            `FilmService.findOneAndUpdate(): update=${JSON5.stringify(update)}`,
+        );
+        
+    }
+
+    async delete(id: string) {
+        if (this.mock !== undefined) {
+            return this.mock.remove(id);
+        }
+        logger.debug(`FilmService.delete(): id=${id}`);
+
+        // Das Film zur gegebenen ID asynchron loeschen
+        const { deletedCount } = await FilmModel.deleteOne({ _id: id }); // eslint-disable-line @typescript-eslint/naming-convention
+        logger.debug(`FilmService.delete(): deletedCount=${deletedCount}`);
+        return deletedCount !== undefined;
+
+        // Weitere Methoden von mongoose, um zu loeschen:
+        //  Film.findByIdAndRemove(id)
+        //  Film.findOneAndRemove(bedingung)
+    }
+
+    private async validateCreate(film: Film) {
+        const msg = validateFilm(film);
+        if (msg !== undefined) {
+            logger.debug(
+                `FilmService.validateCreate(): Validation Message: ${JSON5.stringify(
+                    msg,
+                )}`,
+            );
+            return new FilmInvalid(msg);
+        }
+
+        // statt 2 sequentiellen DB-Zugriffen waere 1 DB-Zugriff mit OR besser
+
+        const resultTitel = await this.checkTitelExists(film);
+        if (resultTitel !== undefined) {
+            return resultTitel;
+        }
+
+        const resultIsbn = await this.checkIsbnExists(film);
+        if (resultIsbn !== undefined) {
+            return resultIsbn;
+        }
+
+        logger.debug('FilmService.validateCreate(): ok');
+        return undefined;
+    }
+
+    private async checkTitelExists(film: Film) {
+        const { titel } =film;
+
+        // Pattern "Active Record" (urspruengl. von Ruby-on-Rails)
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        const tmpId = await FilmModel.findOne({ titel }, { _id: true }).lean<
+            string
+        >();
+        if (tmpId !== null) {
+            logger.debug(
+                `FilmService.checkTitelExists(): _id=${JSON5.stringify(tmpId)}`,
+            );
+            return new TitelExists(titel as string, tmpId);
+        }
+
+        logger.debug('FilmService.checkTitelExists(): ok');
+        return undefined;
+    }
+
+    private async checkIsbnExists(film: Film) {
+        const { isbn } = film;
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        const tmpId = await FilmModel.findOne({ isbn }, { _id: true }).lean<
+            string
+        >();
+
+        if (tmpId !== null) {
+            logger.debug(
+                `FilmService.checkIsbnExists(): film=${JSON5.stringify(tmpId)}`,
+            );
+            return new IsbnExists(isbn as string, tmpId);
+        }
+
+        logger.debug('FilmService.checkIsbnExists(): ok');
+        return undefined;
+    }
+
+    private async sendmail(filmData: FilmData) {
+        if (serverConfig.cloud !== undefined) {
+            // In der Cloud kann man z.B. "@sendgrid/mail" statt
+            // "nodemailer" mit lokalem Mailserver verwenden
+            return;
+        }
+
+        const from = '"Wayne Wonder" <Wayne.Wonders@acme.com>';
+        const to = '"Dora Deamon" <Dora.Deamon@acme.com>';
+        const subject = `Neuer Film ${filmData._id}`;
+        const body = `Der Film namens <strong>${filmData.titel}</strong> ist angelegt`;
+
+        const data: SendMailOptions = { from, to, subject, html: body };
+        logger.debug(`sendMail(): data = ${JSON5.stringify(data)}`);
+
+        try {
+            const nodemailer = await import('nodemailer'); // eslint-disable-line node/no-unsupported-features/es-syntax
+            await nodemailer.createTransport(mailConfig).sendMail(data);
+        } catch (err: unknown) {
+            logger.error(
+                `FilmService.create(): Fehler beim Verschicken der Email: ${JSON5.stringify(
+                    err,
+                )}`,
+            );
+        }
+    }
+
+    private async validateUpdate(film: FilmData, versionStr: string) {
+        const result = this.validateVersion(versionStr);
+        if (typeof result !== 'number') {
+            return result;
+        }
+
+        const version = result;
+        logger.debug(`FilmService.validateUpdate(): version=${version}`);
+        logger.debug(
+            `FilmService.validateUpdate(): film=${JSON5.stringify(film)}`,
+        );
+
+        const validationMsg = validateFilm(film);
+        if (validationMsg !== undefined) {
+            return new FilmInvalid(validationMsg);
+        }
+
+        const resultTitel = await this.checkTitelExists(film);
+        if (resultTitel !== undefined && resultTitel.id !== film._id) {
+            return resultTitel;
+        }
+
+        const resultIdAndVersion = await this.checkIdAndVersion(
+            film._id,
+            version,
+        );
+        if (resultIdAndVersion !== undefined) {
+            return resultIdAndVersion;
+        }
+
+        logger.debug('FilmService.validateUpdate(): ok');
+        return undefined;
+    }
+
+    private validateVersion(versionStr: string | undefined) {
+        if (versionStr === undefined) {
+            const error = new VersionInvalid(versionStr);
+            logger.debug(
+                `FilmService.validateVersion(): VersionInvalid=${JSON5.stringify(
+                    error,
+                )}`,
+            );
+            return error;
+        }
+
+        const version = Number.parseInt(versionStr, 10);
+        if (Number.isNaN(version)) {
+            const error = new VersionInvalid(versionStr);
+            logger.debug(
+                `FilmService.validateVersion(): VersionInvalid=${JSON5.stringify(
+                    error,
+                )}`,
+            );
+            return error;
+        }
+
+        return version;
+    }
+
+    private async checkIdAndVersion(id: string | undefined, version: number) {
+        const filmDb = await FilmModel.findById(id).lean<FilmData>();
+        if (filmDb === null) {
+            const result = new FilmNotExists(id);
+            logger.debug(
+                `FilmService.checkIdAndVersion(): FilmNotExists=${JSON5.stringify(
+                    result,
+                )}`,
+            );
+            return result;
+        }
+
+        const versionDb = filmDb.__v ?? 0;
+        if (version < versionDb) {
+            const result = new VersionOutdated(id as string, version);
+            logger.debug(
+                `FilmService.checkIdAndVersion(): VersionOutdated=${JSON5.stringify(
+                    result,
+                )}`,
+            );
+            return result;
+        }
+
+        return undefined;
+    }
+}
