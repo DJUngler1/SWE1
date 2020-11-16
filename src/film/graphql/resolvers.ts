7f87f26f824250674cf20d1b8318e3144c7f3f02
TobiasHnyk <tobi.hnyk@gmail.com>
2020-11-16 22:01:46 +0100

    EsLint-Fehler behoben

--- src/film/graphql/resolvers.ts
+++ src/film/graphql/resolvers.ts
@@ -5,22 +5,6 @@ import {
     VersionInvalid,
     VersionOutdated,
 } from './../service/errors';
-/*
- * Copyright (C) 2018 - present Juergen Zimmermann, Hochschule Karlsruhe
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 import type { Film } from './../entity';
 import { FilmService } from '../service';
@@ -109,7 +93,7 @@ const logUpdateResult = (
                 result,
             )}`,
         );
-        // TODO hier wird getrickst, um __v als "version" im Resultat zu haben
+        // hier wird getrickst, um __v als "version" im Resultat zu haben
         const updateResult: any = result;
         updateResult.version = result.__v;
     }
